ec01812768eae02b509d1a939c0cbb5c50317ccc
Placid Rodrigues <placidrod@gmail.com>
2018-01-06 05:48:42 +0600

    exported arrow func instead of obj in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
 const path = require('path');
 
-module.exports = {
-  context: __dirname,
-  entry: './js/ClientApp.jsx',
-  devtool: 'cheap-eval-source-map',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  // file resolution priority by extension:
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  },
-  stats: {
-    colors: true,
-    reasons: true,
-    chunks: false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+module.exports = () => {
+  return {
+    context: __dirname,
+    entry: './js/ClientApp.jsx',
+    devtool: 'cheap-eval-source-map',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    // file resolution priority by extension:
+    resolve: {
+      extensions: ['.js', '.jsx', '.json']
+    },
+    stats: {
+      colors: true,
+      reasons: true,
+      chunks: false
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader'
+        }
+      ]
+    }
+  };
 };
