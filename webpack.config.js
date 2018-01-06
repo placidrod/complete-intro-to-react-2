b58b3652aea4880dee9934b663cdea0670fb6719
Placid Rodrigues <placidrod@gmail.com>
2018-01-06 07:14:05 +0600

    enable webpack to lint errors

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,12 @@ module.exports = () => ({
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
         test: /\.jsx?$/,
         loader: 'babel-loader'
       }
