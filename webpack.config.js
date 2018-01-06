6aac6772aa4c795d35f95c3366a7726297a34774
Placid Rodrigues <placidrod@gmail.com>
2018-01-06 06:17:49 +0600

    fixed returning object from arrow func

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,28 @@
 const path = require('path');
-/* eslint-disable */
-module.exports = () => {
-  /* eslint-enable */
-  return {
-    context: __dirname,
-    entry: './js/ClientApp.jsx',
-    devtool: 'cheap-eval-source-map',
-    output: {
-      path: path.join(__dirname, 'public'),
-      filename: 'bundle.js'
-    },
-    // file resolution priority by extension:
-    resolve: {
-      extensions: ['.js', '.jsx', '.json']
-    },
-    stats: {
-      colors: true,
-      reasons: true,
-      chunks: false
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          loader: 'babel-loader'
-        }
-      ]
-    }
-  };
-};
+
+module.exports = () => ({
+  context: __dirname,
+  entry: './js/ClientApp.jsx',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  // file resolution priority by extension:
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+});
