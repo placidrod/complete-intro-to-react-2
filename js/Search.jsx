03d1853f1f2adbf8a1c70a512705a240231641a4
Placid Rodrigues <placidrod@gmail.com>
2018-01-08 09:55:26 +1300

    ShowCard added

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import preload from '../data.json';
+import ShowCard from './ShowCard';
 
 const Search = () => (
   <div className="search">
-    <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
+    {preload.shows.map(show => <ShowCard show={show} key={show.imdbID} />)}
   </div>
 );
 
