c72c1b1c8b07541162ee7af67f29f40c5b01624d
Placid Rodrigues <placidrod@gmail.com>
2018-01-08 20:52:47 +1300

    manage first state

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () => (
-  <div className="search">
-    <header>
-      <h1>svideo</h1>
-      <input type="text" placeholder="Search" />
-    </header>
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-    </div>
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: 'my debug statement'
+    };
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+
+  handleSearchTermChange(event) {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            type="text"
+            value={this.state.searchTerm}
+            onChange={this.handleSearchTermChange}
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
