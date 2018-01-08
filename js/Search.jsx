c67537daa5e20ddb663c4a5d4cad44443d902833
Placid Rodrigues <placidrod@gmail.com>
2018-01-08 20:38:33 +1300

    header added

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,6 +4,10 @@ import ShowCard from './ShowCard';
 
 const Search = () => (
   <div className="search">
+    <header>
+      <h1>svideo</h1>
+      <input type="text" placeholder="Search" />
+    </header>
     <div>
       {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
     </div>
