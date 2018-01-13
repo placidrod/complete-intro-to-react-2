c4e17ca4b11bcb4753620f914ab6df40e59ba922
Placid Rodrigues <placidrod@gmail.com>
2018-01-13 18:01:21 +1300

    sent show to Details

--- js/App.jsx
+++ js/App.jsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import type { Match } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 import Details from './Details';
 import FlowConfigDoc from './FlowConfigDoc';
 import FourOhFour from './404';
+import preload from '../data.json';
 
 const App = () => (
   <BrowserRouter>
@@ -14,7 +16,13 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
-        <Route path="/details/:id" component={Details} />
+        <Route
+          path="/details/:id"
+          component={(props: { match: Match }) => {
+            const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
+            return <Details show={selectedShow} {...props} />;
+          }}
+        />
         <Route path="/flow" component={FlowConfigDoc} />
         <Route component={FourOhFour} />
       </Switch>
