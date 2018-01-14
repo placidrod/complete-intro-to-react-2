c4a87cfc39cb5c830ae0c6921b9c874932dcb437
Placid Rodrigues <placidrod@gmail.com>
2018-01-14 15:02:41 +1300

    flow added custom type 'Show' in types.js

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -1,5 +1,15 @@
 // @flow
 
+export type Show = {
+  title: string,
+  poster: string,
+  rating: string,
+  year: string,
+  description: string,
+  imdbID: string,
+  trailer: string
+};
+
 declare var module: {
   hot: {
     accept(path: string, callback: () => void): void
