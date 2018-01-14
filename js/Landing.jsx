176ef39dbb0fb0a24469937c41f228fb186fef37
Placid Rodrigues <placidrod@gmail.com>
2018-01-15 07:48:40 +1300

    redux: form submit in Landing

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,17 +1,38 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import type { RouterHistory } from 'react-router-dom';
 import { setSearchTerm } from './actionCreators';
 
-const Landing = (props: { searchTerm: string, handleSearchTermChange: Function }) => (
-  <div className="landing">
-    <h1>svideo</h1>
-    <input value={props.searchTerm} onChange={props.handleSearchTermChange} type="text" placeholder="Search" />
-    <Link to="/search">Or browse all</Link>
-  </div>
-);
+class Landing extends Component {
+  props: {
+    searchTerm: string,
+    handleSearchTermChange: Function,
+    history: RouterHistory
+  };
+  goToSearch = (event: SyntheticEvent) => {
+    event.preventDefault();
+    this.props.history.push('./search');
+  };
+  render() {
+    return (
+      <div className="landing">
+        <h1>svideo</h1>
+        <form onSubmit={this.goToSearch}>
+          <input
+            value={this.props.searchTerm}
+            onChange={this.props.handleSearchTermChange}
+            type="text"
+            placeholder="Search"
+          />
+        </form>
+        <Link to="/search">Or browse all</Link>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({ searchTerm: state.searchTerm });
 
