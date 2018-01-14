9b7c250ee3d9e46be34a1b1d4958add9c8dad0ab
Placid Rodrigues <placidrod@gmail.com>
2018-01-14 15:09:25 +1300

    flow: added flow type in Search.spec

--- js/__test__/Search.spec.jsx
+++ js/__test__/Search.spec.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import { shallow } from 'enzyme'; // eslint-disable-line
 import preload from '../../data.json';
@@ -5,18 +7,18 @@ import Search from '../Search';
 import ShowCard from '../ShowCard';
 
 test('Search renders correctly', () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   expect(component).toMatchSnapshot();
 });
 
 test('Search renders correct number of shows', () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
 test('Search renders correct number of shows based on search term', () => {
   const searchTerm = 'black';
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   component.find('input').simulate('change', { target: { value: searchTerm } });
   const showCount = preload.shows.filter(show => `${show.title} ${show.description}`.toLowerCase().includes(searchTerm))
     .length;
