76483057e3b5dd59163aa4bca9d54861c4b16756
Placid Rodrigues <placidrod@gmail.com>
2018-01-14 15:08:48 +1300

    ajax implemented for getting backend data

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,12 +1,51 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Header from './Header';
+import Spinner from './Spinner';
 
-const Details = props => (
-  <div className="details">
-    <h1>This is details</h1>
-    <pre><code>{JSON.stringify(props, null, 2)}</code></pre>
-  </div>
-);
+class Details extends Component {
+  state = {
+    apiData: { rating: '' }
+  };
+  componentDidMount() {
+    axios.get(`http://localhost:3000/${this.props.show.imdbID}`).then((response: { data: { rating: string } }) => {
+      this.setState({ apiData: response.data });
+    });
+  }
+  props: {
+    show: Show
+  };
+  render() {
+    const { title, description, year, poster, trailer } = this.props.show;
+    let ratingComponent;
+    if (this.state.apiData.rating) {
+      ratingComponent = <h3>{this.state.apiData.rating}</h3>;
+    } else {
+      ratingComponent = <Spinner />;
+    }
+    return (
+      <div className="details">
+        <Header />
+        <section>
+          <h1>{title}</h1>
+          <h2>({year})</h2>
+          {ratingComponent}
+          <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
+          <p>{description}</p>
+        </section>
+        <div>
+          <iframe
+            title="YouTube Video Frame"
+            src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+            frameBorder="0"
+            allowFullScreen
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
